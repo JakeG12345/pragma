959fea1825b97b61dcddd077eb46da6201614bc1
JakeG12345 <81908799+JakeG12345@users.noreply.github.com>
2022-08-01 18:26:53 +1000

    accounts data fetching in context

--- contexts/AccountsContext.js
+++ contexts/AccountsContext.js
@@ -1,12 +1,50 @@
-import { createContext } from 'react';
+import { createContext, useEffect, useState } from "react"
+import { useMoralisWeb3Api, useMoralisWeb3ApiCall } from "react-moralis"
+import { userdataAddress } from "../helpers/info"
+import abi from "../helpers/userdataAbi.json"
 
 export const AccountsContext = createContext()
 
 export const AccountsProvider = ({ children }) => {
+  const { native } = useMoralisWeb3Api()
+  const [accountsData, setAccountsData] = useState([])
 
-    return (
-        <AccountsContext.Provider value='hey'>
-            {children}
-        </AccountsContext.Provider>
+  const getAccountsDataOptions = {
+    chain: "mumbai",
+    address: userdataAddress,
+    function_name: "getBulkUserData",
+    abi: abi,
+  }
+
+  const { fetch, data, error, isLoading } = useMoralisWeb3ApiCall(
+    native.runContractFunction,
+    { ...getAccountsDataOptions }
+  )
+
+  const addAccountData = (addresses) => {
+    const newAddresses = addresses.filter(
+      (address) => !accountsData.includes(address)
     )
+
+    const options = {
+        ...getAccountsDataOptions,
+        params: { addresses: newAddresses }
+    }
+
+    fetch({ params: options })
+
+    newAddresses.map((e) => {
+      setAccountsData((prev) => [...prev, e])
+    })
+  }
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
+  return (
+    <AccountsContext.Provider value={{ accountsData, addAccountData }}>
+      {children}
+    </AccountsContext.Provider>
+  )
 }
--- pages/Test.js
+++ pages/Test.js
@@ -2,10 +2,13 @@ import React, { useContext } from 'react'
 import { AccountsContext } from '../contexts/AccountsContext'
 
 const Test = () => {
-  const test = useContext(AccountsContext)
+  const accounts = useContext(AccountsContext)
 
   return (
-    <div>{test}</div>
+    <div>
+      <div onClick={() => console.log(accounts.accountsData)}>Log Accounts</div>
+      <div onClick={() => accounts.addAccountData(['0x371fafd40757b82ee3d9139475c2814c1da1a2c5', '0x27a1E7C34eD5977469B902c59f39D179f17A24e5'])}>Update Data</div>
+    </div>
   )
 }
 
